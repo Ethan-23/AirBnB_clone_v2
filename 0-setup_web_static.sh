2942862b82d520e5bf8a406410e034b720564a23
Ethan-23 <emanroberts@gmail.com>
2021-04-16 01:44:14 +0000

    added task 0

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+#comment
+
+apt-get update
+apt-get install nginx
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@89-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+rm -f /data/web_static/current
+ln -s /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "/listen \[::\]:80 default_server/a $listen" /data/web_static/current/
+sudo service nginx restart
