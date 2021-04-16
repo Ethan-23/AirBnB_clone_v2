0d49e6c18c2f61bdb995e6e06f79c401bd154877
Ethan-23 <emanroberts@gmail.com>
2021-04-16 03:35:13 +0000

    added task 2 not done

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Comment"""
+from fabric.api import local
+from datetime import datetime
+
+
+def do_pack():
+    """Fabric .tgz archive"""
+
+    name = datetime.now().strftime("%Y%m%d%H%M%S")
+    local("mkdir -p versions")
+    try:
+        local("tar -cvzf versions/web_static_{}.tgz web_static"
+              .format(name))
+    except:
+        return None
+    return "versions/web_static_{}.tgz web_static".format(name)
+
+def do_deploy(archive_path):
+    """Comment for time!"""
+    if os.path.exists(archive_path):
+        return False
+        
