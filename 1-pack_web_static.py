2141cda6f41bcaf03e4d39ed485744629e16046e
Ethan-23 <emanroberts@gmail.com>
2021-04-16 03:28:49 +0000

    corrected long line

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -7,9 +7,10 @@ def do_pack():
     """Fabric .tgz archive"""
 
     name = datetime.now().strftime("%Y%m%d%H%M%S")
-    local('mkdir -p versions')
+    local("mkdir -p versions")
     try:
-        local("tar -cvzf versions/web_static_{}.tgz web_static".format(name))
+        local("tar -cvzf versions/web_static_{}.tgz web_static"
+              .format(name))
     except:
         return None
     return "versions/web_static_{}.tgz web_static".format(name)
