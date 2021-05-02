701c401ba4fd7ddc662dc2a25a73146e628fe09d
Ethan-23 <emanroberts@gmail.com>
2021-05-02 03:07:16 +0000

    added task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Flask setup"""
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Test command"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """HBNB"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    """HBNB"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python(text='is cool'):
+    """HBNB"""
+    return 'Python {}'.format(text.replace('_', ' '))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
