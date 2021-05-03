86e2344e202c0f46e0b5558f3e456869b37ad2c1
Ethan-23 <emanroberts@gmail.com>
2021-05-03 02:50:36 +0000

    added task 10

---
+++ web_flask/9-states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Flask states"""
+
+from models import storage
+from models.state import State
+from flask import render_template
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def hello_state():
+    """gets states for html"""
+    state = storage.all(State)
+    return render_template('9-states.html', states=state, switch=0)
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def hello_state_id(id=None):
+    """gets states for html"""
+    state = storage.all(State)
+    city = storage.all(City)
+    name = none
+    for i in storage.all(State):
+        if i.id == id:
+            name = i.name
+    return render_template('9-states.html', states=state, cities=city,
+                           ids=id, switch=1 name=name)
+
+
+@app.teardown_appcontext
+def close_world(self):
+    """close"""
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
